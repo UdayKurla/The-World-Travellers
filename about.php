da7ecc787d0f49d0c289dd5e7c4c816665ea1f10
UdayKurla <105578174+UdayKurla@users.noreply.github.com>
2023-04-25 22:06:19 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+   
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<section class="header">
+
+   <a href="home.php" class="logo"></a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p><b>Honest prices</b>,<b>Reliable partners</b>,<b>Quality Guarantee</b>,</p>
+      <p><b>Full Service</b>,
+      <b>Full support</b>,
+      <b>Document processing</b></p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="heading-title"> clients reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Great communication, booking through Travel Online was a breeze. Received a tote bag on arrival which really came in handy throughout the holiday - definitely a nice touch!</p>
+            <h3>Saketh</h3>
+            <span>traveler</span>
+            <img src="images/propic.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>The service we received from shameen was fantastic, if only we could say as much for the flights from jetstar wont fly with them again.</p>
+            <h3>Uday</h3>
+            <span>traveler</span>
+            <img src="images/propic.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Great service, had an issue when our flighta changed but was quickly resolved. Hotel was un aware of some of our inclusion but was quickly fixed up once we showed them the travel vouchers provided.</p>
+            <h3>Varshith</h3>
+            <span>traveler</span>
+            <img src="images/propic.jpg" alt="">
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +919564615158 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +917569243720  </a>
+         <a href="mailto: teamsuv192122@gmail.com"> <i class="fas fa-envelope"></i> teamsuv192122@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Tirupati, India - 517102 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>SUV</span> | all rights reserved! </div>
+
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ book_form.php
@@ -0,0 +1,27 @@
+<?php
+
+   
+$connection = mysqli_connect('localhost','root','','book_db');
+
+   if(isset($_POST['send'])){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $address = $_POST['address'];
+      $location = $_POST['location'];
+      $guests = $_POST['guests'];
+      $arrival = $_POST['arrival'];
+      $leaving = $_POST['leaving'];
+
+      $request = " INSERT into book_form(name, email, phone, address, location, guests, arrival, leaving) VALUES('$name','$email','$phone','$address','$location','$guests','$arrival','$leaving') ";
+      mysqli_query($connection, $request);
+
+      header('location:book.php');
+
+   }
+   
+   else{
+      echo 'Something went wrong. Please try again!';
+   }
+
+?>
